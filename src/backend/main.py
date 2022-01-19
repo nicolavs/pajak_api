b01f4537c78fac4e743574b315a9de8daac544f3
Nicola Vitaly <vitaly@bangunindo.com>
2022-01-19 17:01:32 +0700

    config

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,3 +1,3 @@
 from rest_api.app import app
 
-app.run(debug=True)
+app.run()
--- src/backend/rest_api/app.py
+++ src/backend/rest_api/app.py
@@ -1,10 +1,14 @@
+import os
+
 from flask import Flask
 from rest_api.api import api, blueprint
 from rest_api.controller.todo_controller import api as todo_ns
+from rest_api.config import config_by_name
 
 # add namespaces
 api.add_namespace(todo_ns, path="/todo")
 
 app = Flask(__name__)
+app.config.from_object(config_by_name[os.getenv("ENVIRONMENT") or "dev"])
 app.register_blueprint(blueprint)
 app.app_context().push()
